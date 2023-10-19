ef208f73466d58580a04b0bfc45c352c521edba3
Maciej Olszewski <maciek.ol@op.pl>
2023-10-19 19:22:40 +0200

    Initial commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
  $(document).ready(function() {
-  const apiRoot = 'https://maciej-olszewski-kodilla-ownproject.arktosarktos.repl.co/v1/tasks';
+  const apiRoot = 'http://localhost:8080/v1/tasks/createTask';
   const trelloApiRoot ='https://api.trello.com/1';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
